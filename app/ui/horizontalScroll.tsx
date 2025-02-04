7fddb21192f5da7399cb5e0eba83499fa3d6fba1
Cristian <ctapia.macaya@gmail.com>
2025-02-04 19:46:23 -0300

    h-scroll working

--- app/ui/horizontalScroll.tsx
+++ app/ui/horizontalScroll.tsx
@@ -1,21 +1,30 @@
 "use client";
 
 export default function HorizontalScroll() {
+  const optionsArray = [
+    "Option 1",
+    "Option 2",
+    "Option 3",
+    "Option 4",
+    "Option 5",
+    "Option 6",
+    "Option 7",
+  ];
+
   return (
-    <div className="h-screen flex flex-col items-center p-6">
-      <h1 className="text-2xl font-bold mb-4">Scroll Horizontal</h1>
-      <div className="w-full max-w-4xl overflow-x-auto whitespace-nowrap border rounded-lg p-4 scrollbar-hide">
-        <div className="flex space-x-4">
-          {Array.from({ length: 10 }).map((_, index) => (
-            <div
-              key={index}
-              className="w-40 h-40 flex items-center justify-center bg-gray-200 rounded-lg shadow hover:bg-gray-300 transition"
-            >
-              Item {index + 1}
-            </div>
-          ))}
-        </div>
+    // <div className="h-screen flex flex-col items-center p-6">
+    <div className="w-full max-w-4xl overflow-x-auto whitespace-nowrap border rounded-lg p-4 scrollbar-hide">
+      <div className="flex space-x-4">
+        {optionsArray.map((options) => (
+          <div
+            key={options}
+            className="w-40 h-40 flex items-center justify-center bg-red-200 rounded-lg"
+          >
+            {options}
+          </div>
+        ))}
       </div>
     </div>
+    // </div>
   );
 }
