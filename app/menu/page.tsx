8335eb9829ba1d53b75a41a54e4d51e88b771756
Cristian <ctapia.macaya@gmail.com>
2025-02-05 21:42:35 -0300

    Outside padding fixed for all divs

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -4,10 +4,10 @@ import Products from "../ui/Products";
 export default function Menu() {
   return (
     <div className="grid grid-rows-[10%,80%,10%] h-screen">
-      <div className="overflow-x-auto scrollbar-hide text-center bg-red-600">
+      <div className="p-4 overflow-x-auto scrollbar-hide text-center bg-red-600">
         <Categories />
       </div>
-      <div className="p-4 overflow-y-auto h-full">
+      <div className="p-4 overflow-y-auto">
         <Products />
       </div>
       <footer className="bg-red-700 text-center grid grid-cols-4 w-full items-center">
