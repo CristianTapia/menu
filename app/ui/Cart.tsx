8e48c46d60a053529e8e4c8be0103c344624f031
Cristian <ctapia.macaya@gmail.com>
2025-02-10 18:37:57 -0300

    OffCanvas version cleaned

--- app/ui/Cart.tsx
+++ app/ui/Cart.tsx
@@ -6,11 +6,6 @@ export default function Cart() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    // <div>
-    //   <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
-    //     Carrito
-    //   </button>
-    // </div>
     <div>
       <div>
         <button
