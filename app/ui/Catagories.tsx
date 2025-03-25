2e2d86013096673862cd2568f79940f6e629cdf4
Cristian <ctapia.macaya@gmail.com>
2025-03-25 17:18:58 -0300

    Deleting unused code

--- app/ui/Catagories.tsx
+++ app/ui/Catagories.tsx
@@ -15,25 +15,6 @@ export default function Categories({
     onCategorySelectionAction(category);
   }
   return (
-    // <div className="flex gap-4 h-full">
-    //   <button
-    //     onClick={() => handleCategoryClick(null)}
-    //     className={`px-4 py-2 rounded ${activeCategory === null ? "bg-blue-500 text-white" : "#DC2626"}`}
-    //   >
-    //     Todas
-    //   </button>
-
-    //   {categoriesArray.map((option) => (
-    //     <button
-    //       key={option.id}
-    //       onClick={() => handleCategoryClick(option.name)}
-    //       className={`px-4 py-2 rounded ${activeCategory === option.name ? "bg-blue-500 text-white" : "#DC2626"}`}
-    //     >
-    //       {option.name}
-    //     </button>
-    //   ))}
-    // </div>
-
     <div className="grid grid-cols-[auto_1fr] items-center w-full overflow-x-auto">
       {/* 🔹 Botón "Todas" fijo */}
       <div className="sticky left-0 z-10">
