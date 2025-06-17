b78e2b783fa540e8a3017f44a27bd835013b4746
Cristian <ctapia.macaya@gmail.com>
2025-06-17 18:00:53 -0400

    Adding responsiveness to 320 px screen

--- app/ui/Products.tsx
+++ app/ui/Products.tsx
@@ -39,30 +39,42 @@ export default function Products({
   return (
     <div className="flex flex-col gap-y-4">
       {filteredDishes.map((option) => (
-        <div key={option.id} className="bg-blue-500 text-white text-center grid grid-cols-2">
-          <div className="p-5">{option.name}</div>
-
-          <div className="p-5 grid grid-rows-2 items-center">
-            <div>${option.price}</div>
-            <div className="flex items-center justify-center gap-2">
-              <button className="bg-yellow-400 text-black p-2" onClick={() => minusProd(option.id)}>
-                -
-              </button>
-              <span className="p-2 border">{plus[option.id] || 0}</span>
-              <button className="bg-yellow-400 text-black p-2" onClick={() => plusProd(option.id)}>
-                +
-              </button>
-              <button
-                className="bg-green-400 text-black p-2"
-                onClick={() => {
-                  if ((plus[option.id] || 0) > 0) {
-                    onOrderClickAction(option.name, option.price, plus[option.id], option.category);
-                  }
-                }}
-              >
-                Enviar
-              </button>
+        <div
+          key={option.id}
+          className="bg-blue-500 text-white rounded shadow p-4 flex flex-col sm:grid sm:grid-cols-3 gap-4"
+        >
+          {/* Foto */}
+          <div className="flex items-center justify-center">
+            <div className="w-24 h-24 bg-white border border-gray-300 rounded text-black flex items-center justify-center text-sm">
+              Foto
             </div>
+          </div>
+
+          {/* Nombre + Precio */}
+          <div className="flex flex-col justify-center items-center sm:items-start gap-2">
+            <div className="text-lg font-semibold">{option.name}</div>
+            <div className="text-base">${option.price}</div>
+          </div>
+
+          {/* + - Controles */}
+          <div className="flex flex-wrap justify-center items-center gap-2">
+            <button className="bg-yellow-400 text-black px-3 py-1 rounded" onClick={() => minusProd(option.id)}>
+              -
+            </button>
+            <span className="px-3 py-1 border rounded bg-white text-black">{plus[option.id] || 0}</span>
+            <button className="bg-yellow-400 text-black px-3 py-1 rounded" onClick={() => plusProd(option.id)}>
+              +
+            </button>
+            <button
+              className="bg-green-400 text-black px-3 py-1 rounded"
+              onClick={() => {
+                if ((plus[option.id] || 0) > 0) {
+                  onOrderClickAction(option.name, option.price, plus[option.id], option.category);
+                }
+              }}
+            >
+              Enviar
+            </button>
           </div>
         </div>
       ))}
