dbacbabcdb68d94ce84bdef6eb29d3622f7a1da3
Cristian <ctapia.macaya@gmail.com>
2025-06-28 00:53:51 -0400

    Cleaning code and renaming components

--- app/menu/Menu.tsx
+++ app/menu/Menu.tsx
@@ -1,29 +1,3 @@
-// "use client";
-
-// import { useState } from "react";
-// import Categories from "../ui/Catagories";
-// import Products from "../ui/Products";
-
-// export default function Menu() {
-//   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
-
-//   return (
-//     <div className="grid grid-rows-[auto,1fr,auto] h-screen">
-//       <div className="overflow-x-auto scrollbar-hide text-center grid place-items-center">
-//         <Categories onCategorySelectionAction={setSelectedCategory} />
-//       </div>
-//       <div className="overflow-y-auto flex flex-col p-4 min-h-0">
-//         <Products selectedCategory={selectedCategory} />
-//       </div>
-//       <footer className="bg-red-700 text-center grid grid-cols-2 w-full items-center py-4">
-//         <div className="text-sm">Sugerencias/reclamos</div>
-//         <div className="text-sm">Ubicación</div>
-//       </footer>
-//     </div>
-//   );
-// }
-
-// Menu.tsx
 "use client";
 import { useState } from "react";
 import Categories from "../ui/Catagories";
