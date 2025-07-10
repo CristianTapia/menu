6d99260c53e6f9481540f483531635f67370459c
Cristian <ctapia.macaya@gmail.com>
2025-07-10 17:36:26 -0400

    Converting the /menu page to /

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,54 @@
-import Image from "next/image";
-import Link from "next/link";
+// import Image from "next/image";
+// import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image className="dark:invert" src="/next.svg" alt="logo" width={180} height={38} priority />
-        <p className="list-inside list-decimal text-base text-center sm:text-left">¡Bienvenido/a! 😊</p>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/menu"
-            // target="_blank"
-            rel="noopener noreferrer"
-          >
-            Acceder a la carta
-          </Link>
-          <Link
-            className="rounded-full border border-solid transition-colors flex items-center justify-center text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="/formulario"
-            // target="_blank"
-            rel="noopener noreferrer"
-          >
-            Sugerencias/reclamos
-          </Link>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-1 flex-wrap items-center justify-center text-sm">
-        <span>&copy; 2025. Desarrollado por</span>
-        <a
-          className="flex items-center underline underline-offset-4"
-          href="https://eiper.cl"
-          // target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nombre de la empresa
-        </a>
-      </footer>
-    </div>
-  );
+// export default function Home() {
+//   return (
+//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+//       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+//         <Image className="dark:invert" src="/next.svg" alt="logo" width={180} height={38} priority />
+//         <p className="list-inside list-decimal text-base text-center sm:text-left">¡Bienvenido/a! 😊</p>
+//         <div className="flex gap-4 items-center flex-col sm:flex-row">
+//           <Link
+//             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+//             href="/menu"
+//             // target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Acceder a la carta
+//           </Link>
+//           <Link
+//             className="rounded-full border border-solid transition-colors flex items-center justify-center text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+//             href="/formulario"
+//             // target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Sugerencias/reclamos
+//           </Link>
+//         </div>
+//       </main>
+//       <footer className="row-start-3 flex gap-1 flex-wrap items-center justify-center text-sm">
+//         <span>&copy; 2025. Desarrollado por</span>
+//         <a
+//           className="flex items-center underline underline-offset-4"
+//           href="https://eiper.cl"
+//           // target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Nombre de la empresa
+//         </a>
+//       </footer>
+//     </div>
+//   );
+// }
+
+import ClientMenu from "../app/ui/ClientMenu";
+
+export default async function Page() {
+  const base = process.env.NEXT_PUBLIC_BASE_URL!;
+  const res = await fetch(`${base}/api/products`, {
+    next: { revalidate: 0 },
+  });
+  if (!res.ok) throw new Error(`Error cargando productos: ${res.status}`);
+  const products = await res.json();
+  return <ClientMenu products={products} />;
 }
