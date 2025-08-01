8eeb526549cccc1fa3ecbb1cf092d469a3bb5b0f
Cristian <ctapia.macaya@gmail.com>
2025-08-01 19:04:29 -0400

    Adding colors for light and dark themes automatically

--- app/ui/ClientMenu.tsx
+++ app/ui/ClientMenu.tsx
@@ -24,7 +24,7 @@ export default function ClientMenu({ products, categories }: { products: Product
 
   return (
     <div className="flex flex-col h-screen bg-gray-100">
-      <div className="overflow-x-auto p-4 bg-white shadow-md z-10">
+      <div className="overflow-x-auto p-4 bg-white shadow-md z-10 ">
         <Categories categories={categories} onCategorySelectionAction={setSelectedCategory} />
       </div>
       <main className="flex-1 overflow-y-auto p-4 min-h-0 bg-[#F2F2F2]">
