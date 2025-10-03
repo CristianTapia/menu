42be40c14c6eb1d25037086c7ce8d586f2a09eac
Cristian <ctapia.macaya@gmail.com>
2025-10-03 20:02:47 -0300

    New minimalist card created to show dishes

--- app/ui/Products.tsx
+++ app/ui/Products.tsx
@@ -4,6 +4,7 @@ import Modal from "./Modals/Modal";
 import { useState } from "react";
 import Image from "next/image";
 import { Product } from "@/lib/types";
+import { Info, ShoppingCart } from "lucide-react";
 
 export default function Products({
   products,
@@ -33,11 +34,25 @@ export default function Products({
 
   return (
     <div className="flex flex-col gap-y-4">
+      <div className="rounded p-4 flex flex-row sm:grid-cols-3 gap-2">
+        <div className="w-24 h-24 border border-gray-300 rounded-xl flex items-center justify-center text-sm">Foto</div>
+        <div className="flex flex-col gap-2 items-start">
+          <div className="text-sm font-semibold">Nombre</div>
+          <div className="text-xs text-[var(--color-dish)]">Descripción</div>
+          <div className="text-xs text-[var(--color-dish)]">Popular</div>
+          <div className="text-xs font-extrabold text-[var(--color-primary)]">Precio</div>
+        </div>
+        <div className="flex flex-row gap-2 ml-auto">
+          <button className="place-items-center" aria-label="Más información">
+            <Info color="#EA2831" size={20} />
+          </button>
+          <button className="items-center rounded p-1 bg-[var(--color-primary)]" aria-label="Añadir a la orden">
+            <ShoppingCart color="white" size={20} />
+          </button>
+        </div>
+      </div>
       {sortedProducts.map((product) => (
-        <div
-          key={product.id}
-          className="shadow-xl/10 border border-gray-300 rounded p-4 flex flex-col  sm:grid-cols-3 gap-4"
-        >
+        <div key={product.id} className="rounded p-4 flex flex-col sm:grid-cols-3 gap-4">
           {/* Foto */}
           <div className="flex items-center justify-center">
             {product.image_url ? (
